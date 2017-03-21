e502fbb22a21adfa75cc8d323ce0a2421f3f272b
Simbadev <maxime.gasnier.1@gmail.com>
2017-03-21 22:37:00 +0100

    add correct features

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,11 @@ var Event = require('../models/Events');
 // Get Homepage
 router.get('/', (req, res) => {
 
-  Event.getEvent(err, events) => {
+  Event.getEvent(function() {
       if(err) throw err;
       console.log(events);
     res.render('index');
-  }
+  });
 });
 
 module.exports = router;
