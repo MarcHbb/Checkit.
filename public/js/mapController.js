7655725c8554f84123840b08f3c80be305ae99dd
Simbadev <maxime.gasnier.1@gmail.com>
2017-03-23 14:22:27 +0100

    button for validity address

--- public/js/mapController.js
+++ public/js/mapController.js
@@ -34,7 +34,7 @@ function checkValidityToAddSpot() {
     geocoder.geocode({'address': address}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
-      btn.style.display = "flex";
+      btn.style.display = "inline";
       var marker = new google.maps.Marker({
         map: map,
         position: results[0].geometry.location
